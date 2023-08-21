a948552d8cc258397bf9f753b4e99b0dca2a727e
Dhananjay <79267037+dhannjayyy@users.noreply.github.com>
2023-08-21 18:15:01 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.port || 5500; // You can change this port number if needed
+
+app.use(express.json());
+
+app.get('/search', async (req, res) => {
+  const searchQuery = req.query.q;
+  if (!searchQuery) {
+    return res.status(400).json({ error: 'Missing search query parameter' });
+  }
+
+  const apiUrl = `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${searchQuery}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    res.json(response.data[1]);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching data from the API' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
